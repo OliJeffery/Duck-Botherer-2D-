3920d52d9d2610233b9efb96c144540d2f33ce06
OliJeffery <o.jeffery@gmail.com>
2018-11-29 13:14:50 +0000

    Initial commit

---
+++ Assets/Scripts/Botherer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Botherer : MonoBehaviour {
+
+    [SerializeField] int ScreenWidthInUnits = 16;
+    [SerializeField] float BothererYPos = 0.57f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float CurrentMousePosition = Input.mousePosition.x / Screen.width * ScreenWidthInUnits;
+        gameObject.transform.position = new Vector2(CurrentMousePosition, BothererYPos);
+	}
+}
